7a8a113961b65b85b4e6cc01721160c0527f3a2b
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-26 09:46:19 -0700

    101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints string
+* Description: and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Return 1
+int main(void)
+{
+print("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (1);
+}
