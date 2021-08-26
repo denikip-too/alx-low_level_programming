0347f48360342620cab574c91b46a05120d97da6
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-26 02:17:08 -0700

    6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints size
+* Description: prints the size of various types on the computer it is compiled and run on
+* Return: 0
+*/
+int main(void)
+{
+printf("Size of int %d byte(s) \n", sizeof(int));
+printf("Size of char %d byte(s) \n", sizeof(char));
+printf("Size of float %d byte(s) \n", sizeof(float));
+printf("Size of double %d byte(s) \n", sizeof(double));
+return (0);
+} 
