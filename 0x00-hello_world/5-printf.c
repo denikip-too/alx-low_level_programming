a7a34917764e9e4525fde18c4443d0c1e0ec05ee
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-26 02:01:36 -0700

    5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
 * main - prints string
 * Description : prints with proper grammar, but the outcome is a piece of art,
-* Return 0
+* Return: 0
 */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
