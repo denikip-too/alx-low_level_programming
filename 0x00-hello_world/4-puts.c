3f944659fc8f52f77b1684340edee49b30471685
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-26 01:49:25 -0700

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*C program that prints "Programming is like building a multilingual puzzle*/
+/**
+* main - prints a string
+* Description: C program that prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
