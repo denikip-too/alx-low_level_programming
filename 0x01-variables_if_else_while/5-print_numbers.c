9d3a266dc5d05119a2f1e9b297ddf2dfb7ea714a
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-27 10:45:28 -0700

    5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints numbers
+* Description: prints numbers between 0 & 10
+* Return: 0
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 10; n++)
+printf("%d\n", n);
+return (0);
+}
