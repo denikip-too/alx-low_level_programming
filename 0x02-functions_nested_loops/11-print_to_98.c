d8540df43ad7b9487647f4d94c0f183accf35cd6
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-31 13:09:24 -0700

    11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+/**
+* print_to_98 - prints all natural numbers
+* @y: first number
+*/
+void print_to_98(int n)
+{
+int x;
+if (y <= 98)
+{
+for (x = y; x < 98; x++)
+{
+putchar(x);
+}
+}
+else
+{
+for (x = y; x > 98; x--)
+{
+putchar(x);
+}
+}
+putchar('\n');
+}
