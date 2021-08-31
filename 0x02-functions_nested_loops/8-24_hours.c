3db1f4f9a5fd607fd8f9195ae5d125d6857cef79
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-08-31 11:34:36 -0700

    8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * jack_bauer - prints every minute of the day of Jack Bauer
-* Description: prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+* Description: prints every minute of the day of Jack Bauer, in 24 hrs
 */
 void jack_bauer(void)
 {
