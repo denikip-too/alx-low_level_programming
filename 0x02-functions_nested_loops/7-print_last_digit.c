a1b4da4eff7e5ec949cbe69cbcdf6ddd4e7be569
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-01 13:07:24 -0700

    7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,11 @@ int last_digit;
 if (n >= 0)
 {
 last_digit = n % 10;
-_putchar(last_digit + 48);
-return (last_digit + 48);
+return (last_digit);
 }
 else
 {
 last_digit = (n * -1) % 10;
-_putchar(last_digit + 48);
-return (last_digit + 48);
+return (last_digit);
 }
 }
