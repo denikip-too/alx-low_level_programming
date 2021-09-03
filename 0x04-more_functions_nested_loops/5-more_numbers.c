26ed6872ba282ddc337a56d3bb5265ddb56568ed
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-03 03:56:16 -0700

    5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,22 +5,14 @@
 void more_numbers(void)
 {
 int a, b;
-a = 1;
-while (a <= 10)
+for (a = 1; a <= 10; a++)
 {
 for (b = 0; b <= 14; b++)
 {
 if (b >= 10)
-{
 _putchar(b / 10 + 48);
 _putchar(b % 10 + 48);
 }
-else
-{
-_putchar(b + 48);
-}
-}
-a++;
 _putchar('\n');
 }
 }
