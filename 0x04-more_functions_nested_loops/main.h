80fe2faa06b7d0ba4453417fb0d715f9341b5744
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-03 01:43:27 -0700

    main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,16 @@
-wget https://raw.githubusercontent.com/gtfrans2re/alx-low_level_programming/main/0x04-more_functions_nested_loops/main.h
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
+
