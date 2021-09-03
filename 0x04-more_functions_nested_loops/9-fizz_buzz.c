5dac9d3893be08cf3a4ee4968bbc90298aaab659
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-03 06:20:56 -0700

    9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ int main(void)
 int num;
 for (num = 1; num <= 99; num++)
 {
-if (num % 3 == 0)
-printf("Fizz ");
+if (num % 15 == 0)
+printf("FizzBuzz ");
 else if (num % 5 == 0)
 printf("Buzz ");
-else if (num % 15 == 0)
-printf("FizzBuzz ");
+else if (num % 3 == 0)
+printf("Fizz ");
 else
 printf("%i ", num);
 }
