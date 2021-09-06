cf0e5a8c111c9ac48fe522167bdc4ecc6a300daa
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-06 08:30:11 -0700

    4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+char *p = s;
+while (*s)
+s++;
+return (s - p);
+}
+/**
+* print_rev - prints string
+* @s: string to be reversed
+*/
+void print_rev(char *s)
+{
+int size = _strlen(s);
+while (size--)
+_putchar(*(s + size));
+_putchar('\n');
+}
+  
