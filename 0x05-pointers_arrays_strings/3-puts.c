f50fc580a572ea75cf34972112c8704a2c0f4d90
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-06 06:15:09 -0700

    3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts - prints a string
+* Description: function that prints a string
+* @str: string to be printed
+*/
+void _puts(char *str)
+{
+while(*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
