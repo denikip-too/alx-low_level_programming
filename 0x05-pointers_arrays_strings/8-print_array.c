cd6046d79fa59f34bddbf54286b4ed245f311fcc
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-06 12:29:44 -0700

    8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,13 @@
  */
 int powB(int base, int power)
 {
-    int i, prod = 1;
+	int i, prod = 1;
 
-    for (i = 0; i < power; i++)
-    {
-        prod = prod * base;
-    }
-    return (prod);
+	for (i = 0; i < power; i++)
+	{
+		prod = prod * base;
+	}
+	return (prod);
 }
 
 /**
@@ -24,20 +24,20 @@ int powB(int base, int power)
  */
 int numLength(int num)
 {
-    int length = 0;
+	int length = 0;
 
-    if (!num)
-    {
-        return (1);
-    }
+	if (!num)
+	{
+		return (1);
+	}
 
-    while (num)
-    {
-        num = num / 10;
-        length += 1;
-    }
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
 
-    return (length);
+	return (length);
 }
 
 /**
@@ -48,43 +48,43 @@ int numLength(int num)
  */
 void putnchar(int num)
 {
-    int length = numLength(num), j = length - 1, k, tmp2, digit1;
+	int length = numLength(num), j = length - 1, k, tmp2, digit1;
 
-    if (num < 0)
-    {
-        _putchar('-');
-        num = -1 * num;
-    }
-    if (num == 0)
-        _putchar(48);
-    else
-    {
-        while (j >= 0)
-        {
-            if (num % powB(10, j) == 0 && j != 0)
-            {
-                _putchar(48 + num / powB(10, j));
-                for (k = j; k > 0 ; k--)
-                {
-                    _putchar(48);
-                }
-                j = -1;
-            }
-            else
-            {
-                digit1 = num / powB(10, j);
-                _putchar(digit1 + 48);
-                tmp2 = num;
-                num -= powB(10, j) * digit1;
-                if (numLength(tmp2) - numLength(num) == 2)
-                {
-                    _putchar(48);
-                    j--;
-                }
-                j--;
-            }
-        }
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -1 * num;
+	}
+	if (num == 0)
+		_putchar(48);
+	else
+	{
+		while (j >= 0)
+		{
+			if (num % powB(10, j) == 0 && j != 0)
+			{
+				_putchar(48 + num / powB(10, j));
+				for (k = j; k > 0 ; k--)
+				{
+					_putchar(48);
+				}
+				j = -1;
+			}
+			else
+			{
+				digit1 = num / powB(10, j);
+				_putchar(digit1 + 48);
+				tmp2 = num;
+				num -= powB(10, j) * digit1;
+				if (numLength(tmp2) - numLength(num) == 2)
+				{
+					_putchar(48);
+					j--;
+				}
+				j--;
+			}
+		}
+	}
 
 }
 
@@ -95,18 +95,18 @@ void putnchar(int num)
  */
 void print_array(int *a, int n)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < n)
-    {
-        putnchar(a[i]);
+	while (i < n)
+	{
+		putnchar(a[i]);
 
-        if (i == n - 1)
-            break;
-        _putchar(',');
-        _putchar(' ');
-        i++;
-    }
-    _putchar('\n');
+		if (i == n - 1)
+			break;
+		_putchar(',');
+		_putchar(' ');
+		i++;
+	}
+	_putchar('\n');
 
 }
