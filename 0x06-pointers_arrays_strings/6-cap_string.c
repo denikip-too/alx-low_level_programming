4236a7aad6142c0929d708cf6895b8b3aa0b5d13
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-09 10:23:03 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,32 @@
 #include "main.h"
 /**
+* isLower - checks if string s in lower case
+* @i: characters
+* Return: 1 if true, 0 if false
+*/
+int isLower(char i)
+{
+return (i >= 97 && i <= 122);
+}
+/**
+* isDelimeter - determines if ascii is delimeter
+* @y: character
+* Return: 1 if true, 0 if false
+*/
+int isDelimeter(char y)
+{
+int i;
+char delimeter[] = " \t\n,.!?\"(){}";
+for (i = 0; i < 12; i++)
+{
+if (y == delimeter[i])
+{
+return (1);
+}
+}
+return (0);
+}
+/**
 * *cap_string - capitalizes all words of a string
 * @s: string to be capitalized
 * Return: string
@@ -7,11 +34,21 @@
 char *cap_string(char *s)
 {
 char *ptr = s;
+int foundDelimit = 1;
 while (*s)
 {
-if (*s >= 97 && *s <= 122)
+if (isDelimeter(*s))
+{
+foundDelimit = 1;
+}
+else if (isLower(*s) && foundDelimit)
 {
 *s -= 32;
+foundDelimit = 0;
+}
+else
+{
+foundDelimit = 0;
 }
 s++;
 }
