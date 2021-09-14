30f281fa0fe669e8a57ad5e6dc108e0e4d729c21
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-14 13:07:42 -0700

    prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - prints a buffer
+ * print_line - prints a buffer
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
- * Description: prints a buffer
+ * 
  * Return: void
  */
 void print_line(char *c, int s, int l)
