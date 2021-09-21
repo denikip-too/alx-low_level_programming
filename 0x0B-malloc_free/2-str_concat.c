0fc5ba523f22d0a37cec53db1b53239a97f1ceab
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-21 11:42:02 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,11 +28,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		return (NULL);
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		return (NULL);
 	}
 	s3 = malloc(n * sizeof(char));
 	if (s3 == NULL)
