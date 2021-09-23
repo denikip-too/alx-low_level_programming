f9a7c4dbdced6bac7b9e0c6b6297ce8b828be097
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-23 14:44:42 -0700

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,20 +10,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *p = dest;
+	unsigned int i;
 
-	while (*src && n--)
+	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
 	}
-	if (!*src)
-	{
-		while (*dest && n--)
-		{
-			*dest++ = 0;
-		}
-	}
-	return (p);
+	return (dest);
 }
 /**
  * *_realloc - reallocates a memory block using malloc and free
