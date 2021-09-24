5add353e92492c910daf0ee63ebbfa2d0c960078
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-24 14:13:34 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,38 +16,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (*(s1 + len1) != '\0')
-	{
 		len1++;
-	}
 	while (*(s2 + len2) != '\0')
-	{
 		len2++;
-	}
 	if (n >= len2)
-	{
 		n = len2;
-	}
 	p = malloc(sizeof(char) * (len1 + n + 1));
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
-	{
 		p[i] = s1[i];
-	}
 	for (j = 0; j < n; i++, j++)
-	{
 		p[i] = s2[j];
-	}
 	p[i] = '\0';
 	return (p);
 }
