d13bf69be983cd4494be1f37eac14723a53272f0
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-24 13:26:25 -0700

    multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Check if input is only numbers
+ * @num: Given input to check
+ *
+ * Return: 1 if a number, 0 if otherwise
+ */
+int _isdigit(int num)
+{
+	if (num < '0' || num > '9')
+		return (0);
+	return (1);
+}
+/**
+ * main - multiplies two positive numbers
+ * Return - result, followed by a new line
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	malloc();
+	if (argc != 3)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit (98);
+	}
+	if (argc == 3)
+	{
+		int mult, num1, num2;
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		if (!(_isdigit(num1) || _isdigit(num2)))
+		{
+			_purchar('E');
+			_purchar('r');
+			_purchar('r');
+			_purchar('o');
+			_purchar('r');
+			_purchar('\n');
+			exit (98);
+		}
+		else
+		{
+			mult = num1 * num2;
+			_putchar(mult);
+		}
+	}
+	return (0);
+}
