23772219f0fd58d0cc52dd9c936be6d7596fd0d8
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-30 08:32:58 -0700

    prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list num;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
