c9ba0f61749b5a1d3d25ea870d4be92e5330eebc
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-09-30 13:30:44 -0700

    main file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,18 +11,31 @@ int main(int argc, char *argv[])
 {
 	register int x, y;
 	int (*p)(int, int);
+	char *i;
 
 	if (argc != 4)
 	{
 		printf("Error\n"), exit(98);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	i = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*i == '/' || *i == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	p = get_op_func(argv[2]);
 	if (!p)
 	{
 		printf("Error\n"), exit(99);
 	}
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
 	printf("%i\n", p(x, y));
 	return (0);
 }
