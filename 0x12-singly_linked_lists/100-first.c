a99b1f3a1d4071536778c33240bddcebe966e5b3
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-07 08:13:18 -0700

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * my_function - my_function before the main function is executed
+ */
+void my_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
