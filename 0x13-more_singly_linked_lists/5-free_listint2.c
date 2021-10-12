aba5ad38406ac0882b6b9907bc92c51ab4ec798b
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-12 02:52:35 -0700

    frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
-	listint_t *next;
+	listint_t *temp;
 
 	if (!head)
 	{
@@ -16,9 +16,9 @@ void free_listint2(listint_t **head)
 	}
 	while (current != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next->next;
+		temp = current;
+		current = temp->next;
+		free(temp);
 	}
 	*head = NULL;
 	head = NULL;
