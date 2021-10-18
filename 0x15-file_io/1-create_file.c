6a95843a8f90d4da2a15b21627012a1ea2a50284
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-18 11:16:12 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, w;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (op == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		w = write(op, text_content, i);
+		if (w == -1)
+		{
+			return (-1);
+		}
+	}
+	close(op);
+	return (1);
+}
