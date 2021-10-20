c9289ff76d0a1db523eb4752337b4e809e5b096d
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-20 12:50:27 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int op, op1, w, r;
+	int op, op1;
+	int w, r;
 	char buf[1024];
 
 	if (argc != 3)
@@ -22,7 +23,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	op1 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	op1 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	r = read(op, buf, 1024);
 	w = write(op1, buf, r);
 	while (r > 0)
@@ -37,11 +38,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(op) < 0)
+	if ((close(op)) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", op), exit(100);
 	}
-	if (close(op1) < 0)
+	if ((close(op1)) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", op1), exit(100);
 	}
