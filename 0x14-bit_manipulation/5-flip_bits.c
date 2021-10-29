ea2b66ddbfcb1e495ee0417c0692844e5b513d4d
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-29 12:19:47 -0700

    function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * countSetBits - count set bits
- * @n: integers
- * Return: return the number of counts
- */
-int countSetBits(int n)
-{
-	int count = 0;
-
-	while (n > 0)
-	{
-		count++;
-		n &= (n - 1);
-	}
-	return (count);
-}
-/**
  * flip_bits - returns the number of bits you would need
  * to flip to get from one number to another
  * @n: first integer
@@ -25,5 +9,14 @@ int countSetBits(int n)
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (countSetBits(n ^ m));
+	unsigned int i;
+	int count = 0;
+
+	i = (n ^ m);
+	while (i)
+	{
+		count++;
+		i &= (i - 1);
+	}
+	return (count);
 }
