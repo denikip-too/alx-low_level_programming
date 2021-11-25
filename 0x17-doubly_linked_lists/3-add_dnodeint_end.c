10f00399c70629fac4b7c65e3c2b6a4cb73ed889
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-11-25 08:40:39 -0800

    adds a new node at the end of a list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,6 +30,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		last = last->next;
 	}
 	last->next = new_node;
-	new_node->next = last;
+	new_node->prev = last;
 	return (*head);
 }
