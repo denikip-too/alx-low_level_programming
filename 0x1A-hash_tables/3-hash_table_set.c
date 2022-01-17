62fa23532f2c4fa76505a88b518f62f4c89ebeaa
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-17 13:36:51 -0800

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)i
+	if (new == NULL)
 	{
 		free(new);
 		return (0);
