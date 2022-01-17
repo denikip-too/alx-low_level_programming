03167886d14af062c0e0d5b4cf541f5077c724cd
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-17 13:17:08 -0800

    fix

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *create_table;
+
+	create_table = malloc(sizeof(hash_table_t));
+	if (create_table == NULL)
+	{
+		return (NULL);
+	}
+	create_table->size = size;
+	create_table->array = calloc(size, sizeof(hash_node_t));
+	if (create_table->array == NULL)
+	{
+		free(create_table);
+		return (NULL);
+	}
+	return (create_table);
+}
