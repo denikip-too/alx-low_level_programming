2f3ae6708f78d28ff5807123a9f84c64f26495eb
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-18 16:21:27 -0800

    fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,14 +7,38 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	size_t i;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
-		{
-			free((void*)ht->array[i]->key);
-		}
+		node = ht->array[i];
+		free_hash_list(node);
 	}
 	free(ht->array);
 	free(ht);
+}
+
+
+/**
+ * free_hash_list - frees a hash_node_t list
+ * @head: head of linked list
+ */
+void free_hash_list(hash_node_t *head)
+{
+	hash_node_t *current;
+	hash_node_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->key);
+		free(current->value);
+		free(current);
+		current = next;
+	}
 }
