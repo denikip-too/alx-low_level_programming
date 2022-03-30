4ec45913bf74e93e9d1581c97baafd234992194b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-30 12:43:51 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,12 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#define min(a, b) ((a < b) ? a : b)
+
 #include <stddef.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
